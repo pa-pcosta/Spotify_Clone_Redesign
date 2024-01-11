1adf1e3c323aa557d1502e087e173fe3aaaca400
PA <pedro.augusto@inventsoftware.com.br>
2024-01-10 21:15:49 -0300

    Aula #02

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private spotifyService: SpotifyService) { }
+
+  ngOnInit(): void {
+  }
+
+  aoClicarEmAbrirMeuSpotify() {
+    //Redireciona para página de login do Spotify com base na URL fornecida
+    //pela chamada do método implementado na classe de serviços injetada
+    
+    window.location.href = this.spotifyService.obterUrlLogin();
+  }
+}
